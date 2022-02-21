1a2d13ce4350fc02048016adf8ba70b2fb7991c2
Ludwigm6 <marvin.ludwig93@gmail.com>
2022-02-21 16:44:31 +0100

    preprocessing

---
+++ openEO/openEO_sentinel.R
@@ -0,0 +1,125 @@
+# Goal is a landcover classification covering all of germany, 
+# including a random forest model training and prediction
+
+# load package
+library(openeo)
+library(sf)
+
+# establish connection
+con <- connect(host = "https://openeo.cloud")
+
+
+collections = list_collections()
+
+
+
+
+# authenticate
+
+
+# get a process graph builder, see ?processes
+p <- processes()
+
+# bigger, for LUCAS testing
+aoi = list(west = 5.6,
+           south = 50.2,
+           east = 8.9,
+           north = 52.5) # nrw
+# smaller, for faster runs
+#aoa <- list(west = 10.452617, south = 51.361166, east = 10.459773, north = 51.364194)
+t <- c("2021-04-01", "2021-10-01")
+
+cube_s2 <- p$load_collection(
+  id = "SENTINEL2_L2A",
+  spatial_extent = aoi,
+  temporal_extent = t,
+  # load less bands for faster computation
+  bands=c("B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12")
+  # bands= c("B02", "B04", "B08")
+  # AFAIK resolution stays at highest by default
+)
+
+# load SCL in separate collection, must be same aoi and t extent!
+cube_SCL <- p$load_collection(
+  id = "SENTINEL2_L2A_SENTINELHUB",
+  spatial_extent = aoi,
+  temporal_extent = t,
+  bands=c("SCL")
+)
+
+
+
+# define filter function to create mask from a cube that only contains 1 band: SCL
+clouds_ <- function(data, context) {
+  SCL <- data[1] # select SCL band
+  # we wanna keep:
+  veg <- p$eq(SCL, 4) # select pixels with the respective codes
+  no_veg <- p$eq(SCL, 5)
+  water <- p$eq(SCL, 6)
+  unclassified <- p$eq(SCL, 7)
+  snow <- p$eq(SCL, 11)
+  # or has only 2 arguments so..
+  or1 <- p$or(veg, no_veg)
+  or2 <- p$or(water, unclassified)
+  or3 <- p$or(or2, snow)
+  # create mask
+  return(p$not(p$or(or1, or3)))
+}
+
+# create mask by reducing bands with our defined formula
+cube_SCL_mask <- p$reduce_dimension(data = cube_SCL, reducer = clouds_, dimension = "bands")
+
+# mask the S2 cube
+cube_s2_masked <- p$mask(cube_s2, cube_SCL_mask) 
+# default: replaced with 0. change to -99?
+
+cube_s2_yearly_composite <- p$reduce_dimension(cube_s2_masked, function(x, context) {
+  p$median(x, ignore_nodata = TRUE)
+}, "t")
+
+# compute indices here, possibly move up when needed per season or something
+# CHANGE BAND INDICES HERE WHEN CHANGING BANDS LOADED
+# ("B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12")
+
+ndvi_ <- function(x, context) {
+  b4 <- x[3]
+  b8 <- x[7]
+  return(p$normalized_difference(b8, b4))
+}
+
+evi_ <- function(x, context) {
+  b2 <- x[1]
+  b4 <- x[3]
+  b8 <- x[7]
+  return((2.5 * (b8 - b4)) / ((b8 + 6 * b4 - 7.5 * b2) + 1))
+}
+
+cube_s2_yearly_ndvi <- p$reduce_dimension(cube_s2_yearly_composite, ndvi_, "bands")
+cube_s2_yearly_ndvi <- p$add_dimension(cube_s2_yearly_ndvi, name = "bands", label = "NDVI", type = "bands")
+
+cube_s2_yearly_evi <- p$reduce_dimension(cube_s2_yearly_composite, evi_, "bands")
+cube_s2_yearly_evi <- p$add_dimension(cube_s2_yearly_evi, name = "bands", label = "EVI", type = "bands")
+
+# merge cubes
+cube_s2_yearly_merge1 <- p$merge_cubes(cube_s2_yearly_composite, cube_s2_yearly_ndvi)
+cube_s2_yearly_merge2 <- p$merge_cubes(cube_s2_yearly_merge1, cube_s2_yearly_evi)
+
+# create result node
+res <- p$save_result(data = cube_s2_yearly_merge2, format = "NetCDF")
+
+# export with option
+# res <- p$save_result(data = cube_s2_yearly_extr, format = "NetCDF", options = list(sample_by_feature = TRUE))
+
+# send job to back-end
+#job <- create_job(graph = res, title = "NRW Sentinel 2")
+
+
+process = as(res, "Process")
+
+
+sink(file = "job")
+process$getProcessGraph()
+sink(NULL)
+
+
+
---
+++ scripts/prepro-clean_reference.R
@@ -0,0 +1,69 @@
+# training data cleaning
+
+library(sf)
+library(dplyr)
+library(mapview)
+library(stringr)
+
+mapview::mapviewOptions(fgb = FALSE) 
+
+samples = st_read("data/trainingsites_remotesensingcourse.gpkg")
+
+
+# Removing the obvious
+samples = samples %>% filter(Kategorie_2 != "Wolken")
+samples = samples %>% filter(Kategorie_2 != "Kulturlandschaft")
+
+
+
+# Aachen is wrong
+samples = samples %>% filter(Region != "Aachen")
+
+samples$Region[samples$Region == "trainData_aachen_kai.RDS"] = "Aachen"
+samples$Region[samples$Region == "Trainingsdaten_Heidelberg_Sophia.RDS"] = "Heidelberg"
+samples$Region[samples$Region == "Noerdliches_Ruhrgebiet"] = "Bottrop"
+
+
+samples = samples %>% filter(Kategorie_2 != "Industrie")
+samples = samples %>% filter(Kategorie_2 != "Offenboden")
+
+samples$polygonID = paste0(samples$Region, str_pad(samples$ID, 2, pad = 0))
+
+samples = samples %>% select(all_of(c("polygonID", "Region", "Kategorie_1", "Kategorie_2")))
+
+samples$Kategorie_1[samples$Kategorie_1 == "Gewässer"] = "water" 
+samples$Kategorie_1[samples$Kategorie_1 == "Industrie"] = "industrial"
+samples$Kategorie_1[samples$Kategorie_1 == "Infrastruktur"] = "infrastructure"
+samples$Kategorie_1[samples$Kategorie_1 == "Küste"] = "coast"
+samples$Kategorie_1[samples$Kategorie_1 == "Offenboden"] = "soil"
+samples$Kategorie_1[samples$Kategorie_1 == "Siedlung "] = "settlement"
+samples$Kategorie_1[samples$Kategorie_1 == "Vegetation"] = "vegetation"
+
+
+samples$Kategorie_2[samples$Kategorie_2 == "Bebauung"] = "settlement"
+samples$Kategorie_2[samples$Kategorie_2 == "Fliessgewaesser"] = "river"
+samples$Kategorie_2[samples$Kategorie_2 == "Gruenland"] = "grassland"
+samples$Kategorie_2[samples$Kategorie_2 == "Landwirtschaft"] = "agriculture"
+samples$Kategorie_2[samples$Kategorie_2 == "Salzwasser"] = "sea"
+samples$Kategorie_2[samples$Kategorie_2 == "Sand"] = "sand"
+samples$Kategorie_2[samples$Kategorie_2 == "See"] = "lake"
+samples$Kategorie_2[samples$Kategorie_2 == "Verkehrswege"] = "roads"
+samples$Kategorie_2[samples$Kategorie_2 == "Wald "] = "forest"
+
+
+samples = samples %>% rename(lcc1 = Kategorie_1,
+                             lcc2 = Kategorie_2,
+                             region = Region)
+
+
+samples$set = "training"
+samples$set[samples$region %in% c("Heidelberg", "Eiderstedt")] = "testing"
+
+
+
+st_write(samples, "data/reference_polygons.gpkg")
+
+
+
+
+
